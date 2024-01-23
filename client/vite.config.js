f55337138236116c72bbd3d1b5dca09903d9a7a9
David <you@example.com>
2024-01-23 16:03:34 +0100

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/url_shortener/',
   plugins: [react()],
+  build:{
+    outDir:'dist',
+    assetsDir:'assets',
+  },
+  publicDir:'static'
 })
